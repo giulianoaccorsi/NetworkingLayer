b5b64f3848be39f3270e12d7e98b420c28e9ef3c
Giuliano <giulianoaccorsi@gmail.com>
2023-06-13 23:23:08 -0300

    Corrige protocolo para fácil manutenção

--- Sources/NetworkingLayer/Protocols/NetworkProtocol.swift
+++ Sources/NetworkingLayer/Protocols/NetworkProtocol.swift
@@ -8,29 +8,53 @@
 import Foundation
 
 public protocol DataRequest {
-    associatedtype Response
-    
+    associatedtype Response: Decodable
+
     var url: String { get }
     var method: HTTPMethod { get }
     var headers: [String : String] { get }
     var queryItems: [String : String] { get }
+    var decoder: JSONDecoder { get }
 
+    func makeURLRequest() throws -> URLRequest
     func decode(_ data: Data) throws -> Response
 }
 
-public extension DataRequest where Response: Decodable {
-    func decode(_ data: Data) throws -> Response {
-        let decoder = JSONDecoder()
-        return try decoder.decode(Response.self, from: data)
-    }
-}
-
 public extension DataRequest {
+
     var headers: [String : String] {
         [:]
     }
 
     var queryItems: [String : String] {
         [:]
+    }
+
+    var decoder: JSONDecoder {
+        JSONDecoder()
+    }
+
+    func makeURLRequest() throws -> URLRequest {
+        guard var urlComponents = URLComponents(string: url) else {
+            throw URLError(.badURL)
+        }
+
+        urlComponents.queryItems = queryItems.map {
+            URLQueryItem(name: $0.key, value: $0.value)
+        }
+
+        guard let finalURL = urlComponents.url else {
+            throw URLError(.badURL)
+        }
+
+        var request = URLRequest(url: finalURL)
+        request.httpMethod = method.rawValue
+        request.allHTTPHeaderFields = headers
+
+        return request
+    }
+
+    func decode(_ data: Data) throws -> Response {
+        try decoder.decode(Response.self, from: data)
     }
 }
