7fda8f3cc01744710000bdcebdd830e963c8df38
Giuliano <giulianoaccorsi@gmail.com>
2023-06-14 20:17:24 -0300

    Melhora código do DefaultNetworkService

--- Sources/NetworkingLayer/NetworkService.swift
+++ Sources/NetworkingLayer/NetworkService.swift
@@ -11,60 +11,26 @@ public protocol NetworkService {
     func request<Request: DataRequest>(_ request: Request) async throws -> Request.Response
 }
 
-
 public final class DefaultNetworkService: NetworkService {
 
     public init() {}
 
     public func request<Request: DataRequest>(_ request: Request) async throws -> Request.Response {
 
-        guard var urlComponent = URLComponents(string: request.url) else {
-            throw NSError(
-                domain: ServiceError.invalidEndpoint.localizedDescription,
-                code: 404,
-                userInfo: nil
-            )
-        }
-
-        var queryItems: [URLQueryItem] = []
-
-        request.queryItems.forEach {
-            let urlQueryItem = URLQueryItem(name: $0.key, value: $0.value)
-            urlComponent.queryItems?.append(urlQueryItem)
-            queryItems.append(urlQueryItem)
-        }
-
-        urlComponent.queryItems = queryItems
-
-        guard let url = urlComponent.url else {
-            throw NSError(
-                domain: ServiceError.invalidEndpoint.localizedDescription,
-                code: 404,
-                userInfo: nil
-            )
-        }
-
-        var urlRequest = URLRequest(url: url)
-        urlRequest.httpMethod = request.method.rawValue
-        urlRequest.allHTTPHeaderFields = request.headers
-
-        guard let urlRequestURL = urlRequest.url else {
-            throw NSError(
-                domain: ServiceError.invalidEndpoint.localizedDescription,
-                code: 404,
-                userInfo: nil
-            )
+        guard let urlRequestURL = try request.makeURLRequest().url else {
+            throw ServiceError.badURL
         }
 
         let (data, response) = try await URLSession.shared.data(from: urlRequestURL)
 
-        guard let httpResponse = response as? HTTPURLResponse, 200..<300 ~= httpResponse.statusCode else {
-            throw NSError(
-                domain: ServiceError.invalidEndpoint.localizedDescription,
-                code: 404,
-                userInfo: nil
-            )
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw ServiceError.noResponse
         }
+
+        guard 200..<300 ~= httpResponse.statusCode else {
+            throw ServiceError.invalidStatusCode(httpResponse.statusCode)
+        }
+
 
         return try request.decode(data)
     }
